6f109dab23d162d7a33e1e418a0d0ef5b40b31f3
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-04-29 22:15:23 +0200

    added render()-function; created cross and circle

---
+++ script.js
@@ -0,0 +1,26 @@
+function init() {
+    render();
+}
+
+function render() {
+    let content = document.getElementById('content');
+    let table = '<table>';
+
+    for (let i = 0; i < 3; i++) {
+        table += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            let index = i * 3 + j;
+            let symbol = fields[index] ? fields[index] : ''; // Der Ausdruck vor dem ? wird ausgewertet. Ist er true, wird der Wert zwischen dem ? und dem : als Wert für die Variable 'symbol' verwendet. Ist er false, wird der Wert nach dem : als Wert für die Variable 'symbol' verwendet, also ein leerer String.
+            if (symbol === 'circle') {
+                symbol = 'o';
+            } else if (symbol === 'cross') {
+                symbol = 'x';
+            }
+            table += `<td>${symbol}</td>`;
+        }
+        table += '</tr>';
+    }
+
+    table += '</table>';
+    content.innerHTML = table;
+}
---
+++ var.js
@@ -0,0 +1,11 @@
+let fields = [
+    null, // Feld 0
+    'circle', // Feld 1
+    'circle', // 2
+    'circle', // 3
+    null, // 4
+    null, // 5
+    'cross', // 6
+    'cross', // 7
+    null, // 8
+];
